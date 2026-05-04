a9950adf004e1500b5f7a575c2a756d5293eb851
Zhuoer Du <111913404+w-dze@users.noreply.github.com>
2026-05-04 15:40:13 -0500

    Add: is-anagram - submission-0

---
+++ Data Structures & Algorithms/is-anagram/submission-0.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #start with one string
+        #go through every letter in the first string
+        #and then go find that same letter in the second string
+        #pop the letter from the word if i found
+        #return true if the second string is empty
+        #and looped through the first string
+        if len(s) != len(t):
+            return False
+        for string in s:
+            if string in t:
+                index = t.index(string)
+                if index != -1:
+                    t = t[:index] + t[index+1:]
+                else:
+                    return False
+            else:
+                return False
+            
+        if t == "":
+            return True
+        else:
+            return False
