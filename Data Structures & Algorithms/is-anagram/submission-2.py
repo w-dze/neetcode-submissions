32eb0794aa37bc61f1fc2babb7da232b381cab86
Zhuoer Du <111913404+w-dze@users.noreply.github.com>
2026-05-11 16:22:05 -0500

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #if the length is not the same
+        #return false immediately
+        if len(s) != len(t):
+            return False
+            
+        #dictionary
+        one = {}
+        two = {}
+
+        for string in s:
+            one[string] = one.get(string, 0) + 1
+        
+        for string in t:
+            two[string] = two.get(string,0) + 1
+
+        return one == two
